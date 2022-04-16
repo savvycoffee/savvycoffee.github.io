149c608d370e7e8f41640ad10c4f8f8945a67d4c
duotonecoffee <andrew@duotone.coffee>
2022-04-16 14:59:08 -0600

    cleanup

--- currency-formatter.js
+++ currency-formatter.js
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
 $("input[data-type='currency']").on({
     keyup: function() {
       formatCurrency($(this));
